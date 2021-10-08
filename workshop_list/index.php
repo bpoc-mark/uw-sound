10ea7423e57e4e51067486bb41a7bd13720dfbee
bpoc-mark <markariel.maata@gmail.com>
2021-10-08 15:42:24 +0800

    added workshop list page pc and sp view

---
+++ workshop_list/index.php
@@ -0,0 +1,204 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/config.php"); ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/tag_manager.php"); ?>
+  <meta charset="UTF-8" />
+  <meta name="robots" content="index,follow">
+  <link rel="canonical" href="<?php echo CANONICAL ?>">
+  <meta name="keywords" content="<?php echo KEY_WORD ?>">
+  <meta name="description" content="<?php echo DESCRIPTION ?>">
+  <meta property="og:title" content="<?php echo SITE_NAME ?>">
+  <meta property="og:type" content="article" /> <!-- 下層では「article」にしてください -->
+  <meta property="og:description" content="<?php echo DESCRIPTION ?>" />
+  <meta property="og:url" content="<?php echo CANONICAL ?>" />
+
+  <title>ワークショップ | <?php echo SITE_NAME ?></title>
+
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_include.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/ga.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/header_tag.php"); ?>
+</head>
+
+<body id="Workshop_list">
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/content_tag.php"); ?>
+  <div id="AllBox">
+    <header>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_box.php"); ?>
+    </header>
+
+    <main>
+      <section class="jumbo_section">
+        <div class="wrapper">
+          <div class="container">
+            <ul class="breadcrumbs pc">
+              <li>
+                <a href="">TOP</a>
+              </li>
+              <li>
+                <a href="">ワークショップ</a>
+              </li>
+            </ul>
+            <h3>ワークショップ</h3>
+              <p>
+                <img src="/images/common/lower_pg_img.svg" alt="">
+              </p>
+          </div>
+        </div>
+      </section>
+      <section class="sect_1">
+        <div class="wrapper">
+          <div class="container">
+            <div class="topics_cont row">
+              <div class="col c-anim-up">
+                <a href="" class="btn_post_row">
+                  <div class="item">
+                    <img src="/images/top/section_3/1.png" alt="">
+                  </div>
+                  <div class="item">
+                    <p class="post_cat"><span>プレイベント</span></p>
+                    <h4 class="post_sub_head">ヤマハ株式会社</h4>
+                    <h3 class="post_title">楽器づくりにチャレンジ！音が出る仕組みもわかる「つくろう、ならそう！」</h3>
+                    <div class="content">
+                      <p>
+                        ■ 体験日時： 12月11日（土）10:30～、15:45～<br>
+                        12月12日（日）10:30～、14:00～
+                      </p>
+                      <p>■ 参加費： 500円</p>
+                      <p>■ 定　員： 各30名</p>
+                      <p>■ 会　場： アクトシティ浜松</p>
+                    </div>
+                  </div>
+                </a>
+                <div class="btn_cont">
+                  <a href="">
+                    一覧へ戻る
+                    <div class="svg_cont">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 600 400" >
+                        <path class="svg_btn" d="" stroke="#000" stroke-width="64" fill="none" />
+                      </svg>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div class="col c-anim-up">
+                <a href="" class="btn_post_row">
+                  <div class="item">
+                    <img src="/images/top/section_3/2.png" alt="">
+                  </div>
+                  <div class="item">
+                    <!-- <p class="post_cat"><span>プレイベント</span></p> -->
+                    <h4 class="post_sub_head">株式会社河合楽器製作所</h4>
+                    <h3 class="post_title">ピアノ大解剖</h3>
+                    <div class="content">
+                      <p>
+                      ■ 体験日時： 12月11日（土）12:15～<br>
+                      12月12日（日）12:15～
+                      </p>
+                      <p>■ 参加費： 無料</p>
+                      <p>■ 定　員： 10名</p>
+                      <p>■ 会　場： アクトシティ浜松</p>
+                    </div>
+                  </div>
+                </a>
+                <div class="btn_cont">
+                  <a href="">
+                    一覧へ戻る
+                    <div class="svg_cont">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 600 400" >
+                        <path class="svg_btn" d="" stroke="#000" stroke-width="64" fill="none" />
+                      </svg>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div class="col c-anim-up">
+                <a href="" class="btn_post_row">
+                  <div class="item">
+                    <img src="/images/top/section_3/3.png" alt="">
+                  </div>
+                  <div class="item">
+                    <!-- <p class="post_cat"><span>プレイベント</span></p> -->
+                    <h4 class="post_sub_head">ローランド株式会社</h4>
+                    <h3 class="post_title">音を組み立ててみよう</h3>
+                    <div class="content">
+                      <p>
+                      ■ 体験日時： 12月11日（土）12:30～、16:00～<br>
+                      12月12日（日）10:45～
+                      </p>
+                      <p>■ 参加費： 無料</p>
+                      <p>■ 定　員： 各10名（親子5組）</p>
+                      <p>■ 会　場： アクトシティ浜松</p>
+                    </div>
+                  </div>
+                </a>
+                <div class="btn_cont">
+                  <a href="">
+                    一覧へ戻る
+                    <div class="svg_cont">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 600 400" >
+                        <path class="svg_btn" d="" stroke="#000" stroke-width="64" fill="none" />
+                      </svg>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div class="col c-anim-up">
+                <a href="" class="btn_post_row">
+                  <div class="item">
+                    <img src="/images/top/section_3/1.png" alt="">
+                  </div>
+                  <div class="item">
+                    <p class="post_cat"><span>プレイベント</span></p>
+                    <h4 class="post_sub_head">ヤマハ株式会社</h4>
+                    <h3 class="post_title">楽器づくりにチャレンジ！音が出る仕組みもわかる「つくろう、ならそう！」</h3>
+                    <div class="content">
+                      <p>
+                        ■ 体験日時： 12月11日（土）10:30～、15:45～<br>
+                        12月12日（日）10:30～、14:00～
+                      </p>
+                      <p>■ 参加費： 500円</p>
+                      <p>■ 定　員： 各30名</p>
+                      <p>■ 会　場： アクトシティ浜松</p>
+                    </div>
+                  </div>
+                </a>
+                <div class="btn_cont">
+                  <a href="">
+                    一覧へ戻る
+                    <div class="svg_cont">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 600 400" >
+                        <path class="svg_btn" d="" stroke="#000" stroke-width="64" fill="none" />
+                      </svg>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <!-- Wordpress -->
+            <div class="page_navigation">
+              <div class="wp-pagenavi">
+                <a class="previouspostslink" href="#" rel="prev">&#x25C0;</a>
+                <span class="current">1</span>
+                <a class="page smaller" href="#">2</a>
+                <a class="page larger" href="#">3</a>
+                <a class="nextpostslink" href="#" rel="next">&#x25B6;</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+    </main>
+
+    <footer>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/footer_box.php"); ?>
+    </footer>
+  </div><!-- //AllBox -->
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/footer_tag.php"); ?>
+
+</body>
+
+</html>
