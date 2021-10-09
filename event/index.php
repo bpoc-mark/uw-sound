2f77ccacfe49ed9925b6159a639c2a3a16457869
bpoc-mark <markariel.maata@gmail.com>
2021-10-09 20:51:16 +0800

    added event page pc and sp view

---
+++ event/index.php
@@ -0,0 +1,123 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/config.php"); ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/tag_manager.php"); ?>
+  <meta charset="UTF-8" />
+  <meta name="robots" content="index,follow">
+  <link rel="canonical" href="<?php echo CANONICAL ?>">
+  <meta name="keywords" content="<?php echo KEY_WORD ?>">
+  <meta name="description" content="<?php echo DESCRIPTION ?>">
+  <meta property="og:title" content="<?php echo SITE_NAME ?>">
+  <meta property="og:type" content="article" /> <!-- 下層では「article」にしてください -->
+  <meta property="og:description" content="<?php echo DESCRIPTION ?>" />
+  <meta property="og:url" content="<?php echo CANONICAL ?>" />
+
+  <title>ステージイベント | <?php echo SITE_NAME ?></title>
+
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_include.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/ga.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/header_tag.php"); ?>
+</head>
+
+<body id="Event">
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/content_tag.php"); ?>
+  <div id="AllBox">
+    <header>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_box.php"); ?>
+    </header>
+
+    <main>
+      <section class="jumbo_section">
+        <div class="wrapper">
+          <div class="container">
+            <ul class="breadcrumbs pc">
+              <li>
+                <a href="/">TOP</a>
+              </li>
+              <li>
+                <a href="/event/">ステージイベント</a>
+              </li>
+            </ul>
+            <h3>ステージイベント</h3>
+              <p>
+                <img src="/images/common/lower_pg_img.svg" alt="">
+              </p>
+          </div>
+        </div>
+      </section>
+      <section class="sect_1">
+        <div class="wrapper">
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <img src="/images/event/img_1.png" alt="">
+              </div>
+              <div class="col">
+                <h3>
+                  オープニングコンサート <br>
+                  明和電機ナンセンスマシーンコンサート in 浜松
+                </h3>
+                <div class="event_info">
+                  <p>■ 日時： 12/11（土）11:00～12:00</p>
+                  <p>■ 入場料： 無料  要申し込み（定員150名）</p>
+                </div>
+                <p>
+                  土佐信道プロデュースによる芸術ユニット。青い作業服を着用し作品を「製品」、ライブを「製品デモンストレーション」と呼ぶなど、日本の高度経済成長を支えた中小企業のスタイルで、様々なナンセンスマシーンを開発しライブや展覧会など、国内のみならず広く海外でも発表。音符の形の電子楽器「オタマトーン」などの商品開発も行う。2016年1月には中国上海の美術館McaMで、初の大規模展覧会を成功させた。2019年3月には秋葉原「東京ラジオデパート」にて明和電機初の公式ショップ「明和電機秋葉原店」をオープン。2021年は北京（中国）、豊川（愛知）で個展を行う。<br>
+                  <a href="https://www.maywadenki.com/" target="_blank">https://www.maywadenki.com/</a>
+                </p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <img src="/images/event/img_1.png" alt="">
+              </div>
+              <div class="col">
+                <h3>
+                  オープニングコンサート <br>
+                  明和電機ナンセンスマシーンコンサート in 浜松
+                </h3>
+                <div class="event_info">
+                  <p>■ 日時： 12/11（土）11:00～12:00</p>
+                  <p>■ 入場料： 無料  要申し込み（定員150名）</p>
+                </div>
+                <p>
+                  土佐信道プロデュースによる芸術ユニット。青い作業服を着用し作品を「製品」、ライブを「製品デモンストレーション」と呼ぶなど、日本の高度経済成長を支えた中小企業のスタイルで、様々なナンセンスマシーンを開発しライブや展覧会など、国内のみならず広く海外でも発表。音符の形の電子楽器「オタマトーン」などの商品開発も行う。2016年1月には中国上海の美術館McaMで、初の大規模展覧会を成功させた。2019年3月には秋葉原「東京ラジオデパート」にて明和電機初の公式ショップ「明和電機秋葉原店」をオープン。2021年は北京（中国）、豊川（愛知）で個展を行う。<br>
+                  <a href="https://www.maywadenki.com/" target="_blank">https://www.maywadenki.com/</a>
+                </p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <img src="/images/event/img_1.png" alt="">
+              </div>
+              <div class="col">
+                <h3>
+                  オープニングコンサート <br>
+                  明和電機ナンセンスマシーンコンサート in 浜松
+                </h3>
+                <div class="event_info">
+                  <p>■ 日時： 12/11（土）11:00～12:00</p>
+                  <p>■ 入場料： 無料  要申し込み（定員150名）</p>
+                </div>
+                <p>
+                  土佐信道プロデュースによる芸術ユニット。青い作業服を着用し作品を「製品」、ライブを「製品デモンストレーション」と呼ぶなど、日本の高度経済成長を支えた中小企業のスタイルで、様々なナンセンスマシーンを開発しライブや展覧会など、国内のみならず広く海外でも発表。音符の形の電子楽器「オタマトーン」などの商品開発も行う。2016年1月には中国上海の美術館McaMで、初の大規模展覧会を成功させた。2019年3月には秋葉原「東京ラジオデパート」にて明和電機初の公式ショップ「明和電機秋葉原店」をオープン。2021年は北京（中国）、豊川（愛知）で個展を行う。<br>
+                  <a href="https://www.maywadenki.com/" target="_blank">https://www.maywadenki.com/</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/footer_box.php"); ?>
+    </footer>
+  </div><!-- //AllBox -->
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/footer_tag.php"); ?>
+
+</body>
+
+</html>
