7633087bb5708b44ad176c2d613dbf1599764f14
bpoc-mark <markariel.maata@gmail.com>
2021-10-09 18:13:38 +0800

    added sdf page pc and sp view

---
+++ sdf/index.php
@@ -0,0 +1,247 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/config.php"); ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/tag_manager.php"); ?>
+  <meta charset="UTF-8" />
+  <meta name="robots" content="index,follow">
+  <link rel="canonical" href="<?php echo CANONICAL ?>">
+  <meta name="keywords" content="<?php echo KEY_WORD ?>">
+  <meta name="description" content="<?php echo DESCRIPTION ?>">
+  <meta property="og:title" content="<?php echo SITE_NAME ?>">
+  <meta property="og:type" content="article" /> <!-- 下層では「article」にしてください -->
+  <meta property="og:description" content="<?php echo DESCRIPTION ?>" />
+  <meta property="og:url" content="<?php echo CANONICAL ?>" />
+
+  <title>SDFとは？ | <?php echo SITE_NAME ?></title>
+
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_include.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/ga.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/header_tag.php"); ?>
+</head>
+
+<body id="Sdf">
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/content_tag.php"); ?>
+  <div id="AllBox">
+    <header>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_box.php"); ?>
+    </header>
+
+    <main>
+      <section class="jumbo_section">
+        <div class="wrapper">
+          <div class="container">
+            <ul class="breadcrumbs pc">
+              <li>
+                <a href="/">TOP</a>
+              </li>
+              <li>
+                <a href="/sdf/">SDFとは？</a>
+              </li>
+            </ul>
+            <h3>SDFとは？</h3>
+              <p>
+                <img src="/images/common/lower_pg_img.svg" alt="">
+              </p>
+          </div>
+        </div>
+      </section>
+      <section class="sect_1">
+        <div class="wrapper">
+          <div class="container">
+            <h3>音</h3>
+            <h4>
+              <p class="c-anim-up">
+                <span>と創る</span><br>
+                <span>コミュニ</span><br>
+                <span>ケーション</span><br>
+                <span>のカタチ</span>
+              </p>
+            </h4>
+            <div class="description">
+              <p>
+                モノと人、時間と空間、ローカルとリモート、自然と人工、過去と今、今と未来、そして音と人。音を通してつながるコミュニケーション。つなげるモノは想 像次第、つなげるカタチも創造次第。 
+              </p>
+              <p>
+                浜松市では、2014年12月にユネスコ創造都市ネットワークの音楽分野へ加盟したことを契機とし、音の多様性と創造性の新たな可能性を求めて、サウンドデザインをキーワードとして、2016年の「サウンドデザインシンポジウム」、「サウンドデザインフェスティバルin浜松2017」、「サウンドデザインファクトリー in 浜松2019」を開催してまいりました。 
+              </p>
+              <p>
+                これまでサウンドデザインを推進してきた集大成として、この度、「サウンドデ ザインフェスティバルin浜松2021」を開催いたします。コロナ禍によりこれまで の生活が見直されるなかで、「音と創るコミュニケーションのカタチ」のテーマの下、誰もが音創りを楽しめる場の創出、リアルとリモートの共存、距離や空間を越えた創造的な音との出会い、創造的人材の発掘・育成・交流につながることを目指して、様々な角度から音の可能性を感じることができます。 
+              </p>
+              <p>
+                イベントの一部の視聴やオンラインコンテンツによりリモートからも参加できるようにするとともに、サテライト会場においても音に関する関連イベントを開催されます。メイン会場となる展示イベントホールでは、これまで浜松市のサウンドデザインを築き上げてきたクリエーターらによるサウンドデザインに関する作 品展示、ステージイベント、ワークショップを行います。
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect_2">
+        <div class="wrapper">
+          <div class="row">
+            <div class="col">
+              <img src="/images/sdf/img_1.png" alt="">
+            </div>
+            <div class="col">
+              <img src="/images/sdf/img_2.png" alt="">
+            </div>
+            <div class="col">
+              <img src="/images/sdf/img_3.png" alt="">
+            </div>
+            <div class="col">
+              <img src="/images/sdf/img_4.png" alt="">
+            </div>
+            <div class="col">
+              <img src="/images/sdf/img_5.png" alt="">
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect_3">
+        <div class="wrapper">
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <h3>OVERVIEW</h3>
+              </div>
+              <div class="col">
+                <div class="list_cont">
+                  <ul class="list row">
+                    <li class="col">
+                      <p>名称</p>
+                    </li>
+                    <li class="col">
+                      <p>サウンドデザインフェスティバル in 浜松2021</p>
+                    </li>
+                  </ul>
+                  <ul class="list row">
+                    <li class="col">
+                      <p>開催日</p>
+                    </li>
+                    <li class="col">
+                      <p>2021年12月11日(土)・<br class="sp">12月12日(日)　2日間</p>
+                    </li>
+                  </ul>
+                  <ul class="list row">
+                    <li class="col">
+                      <p>時間</p>
+                    </li>
+                    <li class="col">
+                      <p>10:00〜17:00</p>
+                    </li>
+                  </ul>
+                  <ul class="list row">
+                    <li class="col">
+                      <p>会場</p>
+                    </li>
+                    <li class="col">
+                      <p>アクトシティ浜松展示イベントホール、（サテライト会場）浜松科学館／浜松市楽器博物館</p>
+                    </li>
+                  </ul>
+                  <ul class="list row">
+                    <li class="col">
+                      <p>主催</p>
+                    </li>
+                    <li class="col">
+                      <p>
+                        はままつ響きの創造プロジェクト実行委員会/浜松市 <br>
+                        公立大学法人静岡文化芸術大学、公益財団法人浜松・浜名湖ツーリズムビューロー、<br>
+                        浜松商工会議所、公益財団法人浜松地域イノベーション推進機構、<br>
+                        公益財団法人浜松国際交流協会、公益財団法人浜松市文化振興財団、浜松市
+                      </p>
+                    </li>
+                  </ul>
+                  <ul class="list row">
+                    <li class="col">
+                      <p>実行<br class="sp">委員長</p>
+                    </li>
+                    <li class="col">
+                      <p>伊藤 修二</p>
+                    </li>
+                  </ul>
+                  <ul class="list row">
+                    <li class="col">
+                      <p>アドバ<br class="sp">イザー</p>
+                    </li>
+                    <li class="col">
+                      <p>三宅 純</p>
+                    </li>
+                  </ul>
+                  <ul class="list row">
+                    <li class="col">
+                      <p>お問い<br class="sp">合わせ</p>
+                    </li>
+                    <li class="col">
+                      <p>
+                      （公財）浜松市文化振興財団　<br class="sp">Tel. <a href="tel:053-451-1151">053-451-1151</a>  <br class="sp">／  Mail. sdf2021@hcf.or.jp
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+                <div class="btn_cont">
+                  <a href="">
+                    <img src="/images/sdf/pdf_icon.svg" class="pdf_icon" alt="">
+                    チラシデータをダウンロードする
+                    <div class="svg_cont">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 600 400" >
+                        <path class="svg_btn" d="" stroke="#000" stroke-width="64" fill="none" />
+                      </svg>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect_4">
+        <div class="wrapper">
+          <div class="container">
+            <h3>MESSAGE</h3>
+            <div class="row">
+              <div class="col">
+                <img src="/images/sdf/msg_1.png" alt="">
+              </div>
+              <div class="col">
+                <h4><span>浜松市長</span><span class="pc">　</span><br class="sp">鈴木 康友</h4>
+                <p>
+                  サウンドデザインフェスティバルin浜松2021の開催にあたり、多くの方々の暖かいご支援とご協力に心から感謝申し上げます。
+                </p>
+                <p>浜松市は2014年12月にユネスコ創造都市ネットワークの音楽分野にアジアで初めて加盟しました。この加盟を契機に、「世界創造都市フォーラムin浜松2015」を始め、「サウンドデザインシンポジウムin浜松2016」「サウンドデザインフェスティバルin浜松2017」「サウンドデザインファクトリーin浜松2019」など、さまざまなサウンドデザイン事業を開催し、サウンドデザインをキーワードとした音の新たな可能性を求めることで、新たな気づきにつながり、創造都市としての広がりを目指してまいりました。こうした事業ができたことは、集積する楽器産業や活発な市民による音楽活動など、本市がこれまで「音楽のまち」として培ってきた地域資源の存在が大きく貢献していると考えております。</p>
+                <p>今回のフェスティバルは、「音と創る コミュニケーションのカタチ」をテーマに、これまでのサウンドデザイン事業の集大成として実施いたします。デジタルの力を活用しつつも、多様な活動を感じられるように、ステージイベント、ワークショップ、展示など多彩なプログラムを通じて、「音」の可能性を感じていただき、一人一人の中で新たな価値の発見へとつながっていただけたら幸いです。</p>
+                <p>本来であれば、本フェスティバルは、昨年度開催する予定でしたが、新型コロナウイルス感染症拡大のため、１年延期いたしました。フェスティバルの開催に際し、ご尽力を賜りました市内外で活躍されるアーティスト、メーカー、クリエイターをはじめとする多くの皆様に深く感謝を申し上げるとともに、「創造都市・浜松」の推進に一層のご支援とご協力を賜りますようお願い申し上げます。</p>
+                <p>結びに、会場で体験・体感いただける内容だけでなく、コロナ禍においても安心してご参加いただけるように、オンラインと会場とのデュアルモードで楽しんでいただけるように準備を進めてまいりました。ご来場いただける皆様におかれましては、感染予防へのご理解とご協力を賜りますことを重ねて申し上げ、あいさつとさせていただきます。</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="wrapper">
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <img src="/images/sdf/msg_2.png" alt="">
+              </div>
+              <div class="col">
+                <h4>
+                  <p>ユネスコ音楽都市はままつ推進事業実行委員会（公益財団法人浜松市文化振興財団 代表理事）</p>
+                  <span>実行委員長</span><span class="pc">　</span><br class="sp">伊藤 修二</h4>
+                <p>サウンドデザインフェスティバルin浜松2021が多くの関係者のご尽力とご協力により開催することができますことに深く感謝申し上げます。</p>
+                <p>浜松市がユネスコ創造都市ネットワーク音楽分野へ加盟したことに伴い設立された「ユネスコ音楽都市はままつ推進事業実行委員会」は、これまで世界創造都市フォーラム（2015年）、楽器メイカーズフェスティバル（2015年）、世界音楽の祭典in浜松（2016年）、サウンドデザインフェスティバルin浜松（2017年）を開催しました。この事業の成果を引き継ぎ、さらに市民の主体的かつ創造的活動を引き出す仕組みを構築するため平成30年に「はままつ響きの創造プロジェクト」が立ち上がりました。「響き」をキーワードとして、新たな価値の創造、人と人の響き合いによる共生社会の実現、異分野アートによる響き合いから生まれる創造性、そして国内外の創造都市と浜松による都市の響き合いを目的とし、クリエイティブダンスコネクション（2018年）、サウンドデザインファクトリー（2019年）と積み重ねてまいりました。しかし、2020年に開催を予定していたサウンドデザインフェスティバルin浜松はコロナウィルス拡大感染防止のため2021年に延期となりました。</p>
+                <p>本実行委員会では、1年延期となったサウンドデザインフェスティバルの内容について、コロナ渦における開催形態や、求められる安全な運営、オンラインを活用した新たなコンテンツなどを検討するため、専門家によるプロジェクト委員会とともに多くの議論を重ねてまいりました。昨今の社会情勢を踏まえ、「音と創るコミュニケーションのカタチ」をテーマとし、音・サウンドを媒体にして人と人がつながり響き合っていく、新たな可能性や創造性との出会いを体感できる場を準備してまいりました。このフェスティバルにて今まで体験した事のない音・サウンド、コミュニケーションのカタチに出会うことにより皆様の創造的活動がさらに活発となり、浜松市がますます発展していくことを願ってやみません。</p>
+                <p>最後に、このフェスティバルの開催に向けご尽力を賜りました関係の皆様に深くお礼を申し上げますとともに、今後も未来へ向けた持続可能な創造的活動へのご支援とご協力を賜りますようお願い申し上げます。</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/footer_box.php"); ?>
+    </footer>
+  </div><!-- //AllBox -->
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/footer_tag.php"); ?>
+
+</body>
+
+</html>
